884cff404ba84e72af4b1d19f5db3183be5a77f5
stanislavdankiv <stanislavdankiv5@gmail.com>
2021-03-09 10:03:34 +0200

    social-network

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.freelogodesign.org/Content/img/logo-samples/flooop.png' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img src='https://a.d-cd.net/5e2a6c8s-960.jpg' /></div>
+            <div>ava + desc</div>
+            <div>
+                My posts
+        <div>
+                    New posts
+        </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
